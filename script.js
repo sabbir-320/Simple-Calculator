90fb769ecca09c82006734427b32c8e33799be63
sabbir-320 <sabbir195661@gmail.com>
2020-08-10 06:47:36 +0600

    some Woring

---
+++ script.js
@@ -0,0 +1,17 @@
+function getHistory() {
+	return document.getElementById("history-value").innerText;
+}
+function printHistory(num) {
+	return document.getElementById("history-value").innerText = num;
+}
+function getOutput() {
+	return document.getElementById("output-value").innerText;
+}
+function printOutput(num) {
+	return document.getElementById("output-value").innerText = getFormattedNumber(num)
+}
+function getFormattedNumber(){
+	var n = Number();
+	var 
+}
+printOutput("99999999")
